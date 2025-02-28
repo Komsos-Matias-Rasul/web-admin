8fecb3befd66fe65e9a40ad8d3dea37f1aaf2bf2
PetrusAriaa <petrusariacr25@gmail.com>
2025-02-28 15:34:09 +0700

    refactor: migrate from NextUI to HeroUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-const {nextui} = require('@nextui-org/theme');
+const {heroui} = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|dropdown|input|ripple|spinner|menu|divider|popover).js"
+    "./node_modules/@heroui/theme/dist/components/(button|dropdown|input|table|ripple|spinner|menu|divider|popover|form|checkbox|spacer).js"
   ],
   theme: {
     extend: {
@@ -15,5 +15,5 @@ module.exports = {
   },
   plugins: [require('@tailwindcss/typography')({
       className: 'editor-typo'
-    }),nextui()],
+    }),heroui()],
 };
