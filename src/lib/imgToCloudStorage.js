bd386483fba3b69ad74fe13713e430fef7ce4e24
PetrusAriaa <petrusariacr25@gmail.com>
2025-04-01 20:02:09 +0700

    fix: create new image upload handle to decouple image naming

--- src/lib/imgToCloudStorage.js
+++ src/lib/imgToCloudStorage.js
@@ -1,7 +1,40 @@
 import { Storage } from "@google-cloud/storage"
-import { v4 as uuidv4 } from "uuid"
 
-export const imgUploader = (buffer, fileName = uuidv4()) => {
+export const uploadImgToCloudStorage = (buffer, fileName) => {
+  const bucketName = process.env.GCLOUD_BUCKET || ""
+  const b_cred = process.env.GOOGLE_CREDENTIALS_BASE64 || ""
+
+  const _cred = JSON.parse(Buffer.from(b_cred, "base64").toString())
+
+  return new Promise((resolve, reject) => {
+    try {
+      const storage = new Storage({
+        projectId: process.env.GCLOUD_PROJECT,
+        credentials: _cred
+      })
+
+      const bucket = storage.bucket(bucketName)
+      const _file = bucket.file(fileName)
+      const stream = _file.createWriteStream()
+
+      stream.on('error', (err) => {
+        console.error('Stream Error:', err)
+      })
+
+      stream.on('finish', () => {
+        console.log(`File uploaded to ${bucketName} as ${fileName}`)
+      })
+
+      stream.end(buffer)
+      resolve(`/api/img?title=${fileName}`)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+// legacy
+export const imgUploader = (buffer, fileName) => {
   const bucketName = process.env.GCLOUD_BUCKET || ""
   const b_cred = process.env.GOOGLE_CREDENTIALS_BASE64 || ""
 
