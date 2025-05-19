4933bb8a7806053a50d537e3f1f965c40e52689a
PetrusAriaa <petrusariacr25@gmail.com>
2025-05-19 15:50:58 +0700

    prod: add image pattern for next image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,16 @@ const nextConfig = {
       bodySizeLimit: '2mb',
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "parokikosambi-backend.vercel.app",
+        port: "",
+        pathname: "/api/img/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
